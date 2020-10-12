839b1600055f21ab5da5898cddbbcece1edf3966
datthaprasad <datthaprasad29@gmail.com>
2020-10-12 12:15:17 +0530

    ready to heroku

--- routes/rest_handler.js
+++ routes/rest_handler.js
@@ -3,7 +3,7 @@ var db=require('../database');
 
 //For Handling GET request by ADMIN
 const table=(req,res,next)=>{
-  var limit=0;
+  var limit=20;
   if(req.query.limit && req.query.limit!=null){
     limit=req.query.limit;
   }
@@ -23,11 +23,11 @@ const table=(req,res,next)=>{
 const store=(req,res,next)=>{
   const {U_ID,E_ID,R_ID}=req.body;
   
-  var sql=`INSERT INTO data values("${U_ID}","${E_ID}",0)`;
+  var sql=`INSERT INTO data values(NULL,"${U_ID}","${E_ID}",0)`;
   db.query(sql, function (err, data, fields) {
     if (err){
       console.log(err.message);
-      res.status(500).send("SOMETHING WENT WRONG")
+      res.status(500).send("SOMETHING WENT WRONG, PLEASE TRY AGIAN LATER")
     } 
     else{  
       if(R_ID && R_ID!=null){
@@ -47,17 +47,24 @@ const store=(req,res,next)=>{
 
 //For Handling GET request by user
 const get_by_user=(req,res,next)=>{
-  var sql='SELECT UserID,EmailID,Referral_Count,(select count(*)+1 from data where Referral_Count>(select Referral_Count from data where UserID="'+req.query.U_ID+'")) as Rank from data where UserID="'+req.query.U_ID+'"';
-  db.query(sql, function (err, data, fields) {
-    if (err){
-       console.log(err.message);
-       res.status(404).json("NOT FOUND");
+  db.query("set @row_number=0",function(error,datas,fields){
+    if(error){
+      res.status(500).json("SOMETHING WENT WRONG");
     }
-    else{  
-      console.log(data);
-        res.status(200).json(data[0]);
+    else{
+      var sql='select rank,referral_count from (select (@row_number:=@row_number+1) as rank,userid,emailid,referral_count from data order by referral_count desc) as newdata where userid="'+req.query.U_ID+'"';
+      db.query(sql, function (err, data, fields) {
+        if (err){
+           console.log(err.message);
+           res.status(404).json("NOT FOUND");
+        }
+        else{  
+            res.status(200).json(data[0]);
+        }
+      });
     }
-  });
+  })
+ 
 }
 
 
