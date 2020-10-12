3c3c655dbaa955e31cb24d3aafe803b7c198adfd
datthaprasad <datthaprasad29@gmail.com>
2020-10-12 12:46:45 +0530

    ready to heroku

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
+require('custom-env').env(true)
 var conn = mysql.createConnection({
-  host: "sql12.freemysqlhosting.net",//'localhost', // Replace with your host name
-  user: 'sql12369897',      // Replace with your database username
-  password: 'Q8tGJP6G33',      // Replace with your database password
-  database: 'sql12369897' // // Replace with your database Name
+  host: process.env.HOST,//'localhost', // Replace with your host name
+  user: process.env.USER_NAME ,      // Replace with your database username
+  password: process.env.PASSWORD ,      // Replace with your database password
+  database:process.env.DATABASE  // // Replace with your database Name
 }); 
 conn.connect(function(err) {
   if (err) console.log(err.message);
